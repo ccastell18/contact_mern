7f3e1a28ddb2e25f2267ccf68d5e046a059b6bb4
Christopher Castellano <c.castellano18@gmail.com>
2020-06-25 14:55:37 -0500

    add contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,15 +24,67 @@ router.get('/', auth, async (req, res) => {
 //route Post api/contact
 //desc add contacts
 //access private
-router.post('/', (req, res) => {
-  res.send('add contacts');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        //id of user adding contact to list
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //route Put api/contact/:id
 //desc update contacts
 //access private
-router.put('/:id', (req, res) => {
-  res.send('update contacts');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  //Build contact object
+  const contactFields = {};
+
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+    //make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return req.status(401).json({ msg: 'Not Authorized' });
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 //route Delete api/contact/:id
 //desc delete contacts
