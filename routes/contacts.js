7e0e8af10376c18cb30a6214fc055d22a13d147b
Christopher Castellano <c.castellano18@gmail.com>
2020-06-26 01:32:57 -0500

    Update and Delete routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -89,8 +89,22 @@ router.put('/:id', auth, async (req, res) => {
 //route Delete api/contact/:id
 //desc delete contacts
 //access private
-router.delete('/:id', (req, res) => {
-  res.send('delete contacts');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+    //make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return req.status(401).json({ msg: 'Not Authorized' });
+    }
+    await Contact.findByIdAndDelete(req.params.id);
+
+    res.json({ msg: 'Contact Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
