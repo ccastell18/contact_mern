8380633743eeccd18ac0326cdb295c052746b8e1
Christopher Castellano <c.castellano18@gmail.com>
2020-07-18 01:51:54 -0500

    private routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layouts/Navbar';
+import Alerts from './components/layouts/Alerts';
 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import Alerts from './components/layouts/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -31,7 +32,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
